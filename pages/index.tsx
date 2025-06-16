a821cd4b9ef2b6309f7879a976eeeb861db14bda
PimPamPum2211 <intervalo.riscos0c@icloud.com>
2025-06-16 01:19:33 -0500

    Use ServiceGrid on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import services from '../data/services.json';
-import { ServiceCard } from '../components/ServiceCard';
-import { Carousel } from '../components/Carousel';
+import { ServiceGrid } from '../components/ServiceGrid';
 import { CategoryTabs } from '../components/CategoryTabs';
 import { useRouter } from 'next/router';
 
@@ -16,24 +15,16 @@ export default function Home() {
   };
 
   return (
-    <div className="p-4">
-      <CategoryTabs categories={categories} active={activeCat} onChange={setActiveCat} />
-      <Carousel>
-        {services
-          .filter((s) => s.category === activeCat)
-          .map((s) => (
-          <div key={s.id} className="w-60 sm:w-72">
-            <ServiceCard
-              id={s.id}
-              name={s.name}
-              icon={s.icon}
-              image={s.image}
-              price={s.basePrice}
-              onSchedule={() => schedule(s.id)}
-            />
-          </div>
-        ))}
-      </Carousel>
+    <div className="p-4 space-y-4">
+      <CategoryTabs
+        categories={categories}
+        active={activeCat}
+        onChange={setActiveCat}
+      />
+      <ServiceGrid
+        services={services.filter((s) => s.category === activeCat)}
+        onSchedule={schedule}
+      />
     </div>
   );
 }
